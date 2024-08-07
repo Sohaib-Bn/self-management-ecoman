2cf46dacdc4c7ca96b7cd980aeeddd8e5a994c63
Sohaib bn <87204575+Sohaib-Bn@users.noreply.github.com>
2024-08-07 10:39:39 +0100

    fix some bugs

--- src/data/centers.js
+++ src/data/centers.js
@@ -1,6 +1,6 @@
 export const CENTERS = [
   { name: "writing", link: "http://shopillo.store/" },
-  { name: "gols", link: "https://www.youtube.com/" },
+  { name: "goals", link: "https://www.youtube.com/" },
   { name: "learning", link: "https://www.facebook.com" },
   { name: "tasks", link: "/" },
   { name: "mindset", link: "/" },
